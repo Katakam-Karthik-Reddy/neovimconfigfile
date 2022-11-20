a52b4da43b0558d37e738804253922da5168a51f
Katakam-Karthik-Reddy <k.karthikreddy2001@gmail.com>
2022-11-20 19:07:35 +0530

    first complete setup

---
+++ lua/karthik/core/keymaps.lua
@@ -0,0 +1,52 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+--main
+keymap.set('i', 'jk', '<ESC>', { noremap = true })
+keymap.set('v', 'jk', '<ESC>', { noremap = true })
+
+-- clear search highlighs 
+keymap.set('n', '<leader>nh', ':nohl<CR>')
+
+-- deletes character but don't copy it to register
+-- keymap.set('n', 'x', '_x')
+
+-- increase and decrease the numbers
+keymap.set('n', '<leader>+' , '<C-a>')
+keymap.set('n', '<leader>-' , '<C-x>')
+
+-- split windows
+keymap.set('n', '<leader>sv', '<C-w>v') -- splits window into vertical mode
+keymap.set('n', '<leader>sh', '<C-w>s') -- splits window into horizontal mode
+keymap.set('n', '<leader>se', '<C-w>=') -- sets width or height equal
+keymap.set('n', '<leader>sx', ':close<CR>') -- closes the split window
+
+-- tab controll
+keymap.set('n', '<leader>to', ':tabnew<CR>')
+keymap.set('n', '<leader>tx', ':tabclose<CR>')
+keymap.set('n', '<leader>tn', ':tabn<CR>')
+keymap.set('n', '<leader>tp', ':tabp<CR>')
+
+-- to maximize and to previous size
+keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>')
+
+-- nvim-tree
+keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+-- diagnostics
+vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+-- The following command requires plug-ins "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", and optionally "kyazdani42/nvim-web-devicons" for icon support
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+-- If you don't want to use the telescope plug-in but still want to see all the errors/warnings, comment out the telescope line and uncomment this:
+-- vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
---
+++ lua/karthik/plugins-setup.lua
@@ -0,0 +1,103 @@
+-- This is to install packer if it not present in our system.
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()  --true if packer was just install our system.
+
+-- This is to install, update and delete packages.
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
+
+
+
+
+return packer.startup(function(use)
+  use('wbthomason/packer.nvim')
+
+  -- lua functions that many plugins use
+  use('nvim-lua/plenary.nvim') 
+  
+  -- preferred colorscheme
+  use('bluz71/vim-nightfly-guicolors')
+
+  -- tmux & split window navigate
+  use('christoomey/vim-tmux-navigator')
+  use('szw/vim-maximizer')
+
+  -- essential plugins
+  use('tpope/vim-surround')
+  use('vim-scripts/ReplaceWithRegister')
+
+  -- commenting with gc
+  use('numToStr/Comment.nvim')
+  
+  -- file explorer
+  use('nvim-tree/nvim-tree.lua')
+
+  -- statusline
+  use('nvim-lualine/lualine.nvim')
+   
+  -- toggleterminal
+  use('akinsho/toggleterm.nvim') 
+  
+  -- fuzzy finding
+  -- use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+  use({ 'nvim-telescope/telescope.nvim', branch = '0.1.x' })
+
+  --mason is a lsp manager
+  -- use 'williamboman/mason.nvim'
+  -- use 'williamboman/mason-lspconfig.nvim'
+  -- lsp installer
+  use('williamboman/nvim-lsp-installer')
+  -- language server
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+ -- For vsnip users.
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+  
+  --paring of brackets in lsp 
+  use 'windwp/nvim-autopairs'
+
+  -- icons in suggestions
+  use('onsails/lspkind.nvim')
+--For luasnip users.
+-- use 'L3MON4D3/LuaSnip'
+-- use 'saadparwaiz1/cmp_luasnip'
+
+-- For ultisnips users.
+ -- use 'SirVer/ultisnips'
+ -- use 'quangnguyen30192/cmp-nvim-ultisnips'
+
+ -- For snippy users.
+ -- use 'dcampos/nvim-snippy'
+ -- use 'dcampos/cmp-snippy'
+ 
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
